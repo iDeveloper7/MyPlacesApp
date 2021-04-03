fe7c13de65b9ad710e9fe542273e2f0731d9cb69
Vladimir Karsakov <Vovaloko93@mail.ru>
2021-04-03 11:44:26 +0300

    first commit

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -32,12 +32,6 @@ class MainTableViewController: UITableViewController {
         cell.locationLabel.text = place.location
         cell.typeLabel.text = place.type
         cell.imageOfPlace.image = UIImage(data: place.imageData!) //св-во не будет nil
-
-//        if place.imageData == nil{
-//            print("imageData = nil")
-//        } else {
-//            cell.imageOfPlace.image = UIImage(data: place.imageData!)
-//        }
         
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
